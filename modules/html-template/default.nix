3e4ba462e03593f9cac7c000c5398c9292fc9570
lucasew <lucas59356@gmail.com>
2022-12-19 13:31:04 -0300

    better defaults
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- modules/html-template/default.nix
+++ modules/html-template/default.nix
@@ -2,11 +2,16 @@
 let
   inherit (lib) types mkOption;
   item-template = builtins.replaceStrings [ "`" ] [ "@backtick@" ] config.html.template.item-template;
-  templateFinal = builtins.replaceStrings [
+  payload-template = builtins.replaceStrings [
     "@json-data@"                               "@item-template@"
   ] [
     (builtins.toJSON config.target.lyraReady)   item-template
   ] (builtins.readFile ./template.html);
+  final-template = ''
+${config.html.template.prelude}
+${payload-template}
+${config.html.template.poslude}
+  '';
 in {
   options.html.template = {
     item-template = mkOption {
@@ -20,7 +25,9 @@ in {
       type = types.str;
       default = ''
         <md-block>
-          # `@flake@` - `@key@`
+          # `@key@`
+
+          `@flake@`
 
           **Type**: @type@
 
@@ -28,8 +35,47 @@ in {
 
           **Default**: `@default@`
 
+          **Declarations**: @declarations@
+
           @description@
         </md-block>
+      '';
+    };
+    prelude = mkOption {
+      description = ''
+        HTML to put before the templated payload.
+
+        This part should include the <!DOCTYPE html> part, head and the body opening.
+      '';
+      type = types.str;
+      default = ''
+        <!DOCTYPE html>
+        <link rel="stylesheet" href="https://unpkg.com/sakura.css/css/sakura.css" type="text/css">
+        <script type="module" src="https://md-block.verou.me/md-block.js"></script>
+        <input id="lyrasearch-search-input" placeholder="Search through %n% options" />
+        <div id="lyrasearch-container"></div>
+        <md-span></md-span> <!-- just to warm up -->
+        <style>
+        input#lyrasearch-search-input {
+          width: 100%;
+        }
+        </style>
+      '';
+    };
+    poslude = mkOption {
+      description = ''
+        HTML to put after the templated payload.
+
+        This part should include the body and html closing tag.
+      '';
+      type = types.str;
+      default = ''
+        <script>
+        (function () {
+          const ref = document.getElementById('lyrasearch-search-input')
+          ref.placeholder = ref.placeholder.replaceAll('%n%', LYRASEARCH_DATA.length)
+        })()
+        </script>
       '';
     };
   };
@@ -37,7 +83,7 @@ in {
     name = "template.html";
     dontUnpack = true;
     installPhase = ''
-      cp ${builtins.toFile "out.html" templateFinal} $out
+      cp ${builtins.toFile "out.html" final-template} $out
     '';
   };
 }
